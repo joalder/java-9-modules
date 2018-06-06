b8f1f2ef04465b2824caa59e1218ff0ec7b70db9
Jonas Alder (taaaljo2) <jonas.alder@swisscom.com>
2018-06-06 16:14:42 +0200

    uncomment tests which do not work modularized

--- evil-hot-dog-stand/src/test/java/io/joalder/hotdog/best/ingredients/SneakyTactics.java
+++ evil-hot-dog-stand/src/test/java/io/joalder/hotdog/best/ingredients/SneakyTactics.java
@@ -1,41 +1,41 @@
 package io.joalder.hotdog.best.ingredients;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//import org.junit.Test;
+//
+//import io.joalder.hotdog.api.HotDog;
+//import io.joalder.hotdog.wholesale.StandardBun;
+//import io.joalder.hotdog.wholesale.StandardSausage;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
-import io.joalder.hotdog.api.HotDog;
-import io.joalder.hotdog.wholesale.StandardBun;
-import io.joalder.hotdog.wholesale.StandardSausage;
-
-public class SneakyTactics {
-
-	@Test
-	public void directAccess() {
-		SecretSauce secretSauce = new SecretSauce();
-
-		HotDog hotDog = new HotDog(new StandardBun(), new StandardSausage(), secretSauce);
-
-		assertEquals(45, hotDog.getQuality());
-	}
-
-	@Test
-	public void directAccessFullyQualified() {
-		SecretSauce secretSauce = new SecretSauce();
-
-		HotDog hotDog = new HotDog(new StandardBun(), new StandardSausage(), secretSauce);
-
-		assertEquals(45, hotDog.getQuality());
-	}
-
-	@Test
-	public void loadByClassLoader() throws Exception {
-
-		Class clazz = SneakyTactics.class.getClassLoader().loadClass("io.joalder.hotdog.best.ingredients.SecretSauce");
-		SecretSauce secretSauce = (SecretSauce) clazz.getConstructor().newInstance();
-
-		HotDog hotDog = new HotDog(new StandardBun(), new StandardSausage(), secretSauce);
-
-		assertEquals(45, hotDog.getQuality());
-	}
-}
+//public class SneakyTactics {
+//
+//	@Test
+//	public void directAccess() {
+//		SecretSauce secretSauce = new SecretSauce();
+//
+//		HotDog hotDog = new HotDog(new StandardBun(), new StandardSausage(), secretSauce);
+//
+//		assertEquals(45, hotDog.getQuality());
+//	}
+//
+//	@Test
+//	public void directAccessFullyQualified() {
+//		SecretSauce secretSauce = new SecretSauce();
+//
+//		HotDog hotDog = new HotDog(new StandardBun(), new StandardSausage(), secretSauce);
+//
+//		assertEquals(45, hotDog.getQuality());
+//	}
+//
+//	@Test
+//	public void loadByClassLoader() throws Exception {
+//
+//		Class clazz = SneakyTactics.class.getClassLoader().loadClass("io.joalder.hotdog.best.ingredients.SecretSauce");
+//		SecretSauce secretSauce = (SecretSauce) clazz.getConstructor().newInstance();
+//
+//		HotDog hotDog = new HotDog(new StandardBun(), new StandardSausage(), secretSauce);
+//
+//		assertEquals(45, hotDog.getQuality());
+//	}
+//}
